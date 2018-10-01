4b401191635dfc4c4f52e93a0e72ed55aaba2ada
Hugo <hugo.carregno@gmail.com>
2018-10-01 15:51:17 -0300

    agrego huesped

---
+++ src/elgranhotel/Huesped.java
@@ -0,0 +1,61 @@
+
+package elgranhotel;
+
+
+public class Huesped {
+    private String nombre;
+    private long dni;
+    private String domicilio;
+    private String correo;
+    private String celular;
+
+    public Huesped(String nombre, long dni, String domicilio, String correo, String celular) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.correo = correo;
+        this.celular = celular;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+    
+}
