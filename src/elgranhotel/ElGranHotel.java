ae89c5d9e6c161e942e1327ed0e51bffe73edcc4
hugo <hugo@hugo-HP-G42-Notebook-PC>
2018-10-08 15:14:57 -0300

    Aca dejo en el main listo para que puedan crear huesped y verlo en la base de datos

--- src/elgranhotel/ElGranHotel.java
+++ src/elgranhotel/ElGranHotel.java
@@ -5,6 +5,7 @@
  */
 package elgranhotel;
 
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,8 +29,33 @@ public class ElGranHotel {
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ElGranHotel.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+     //Vemos que funcione todo bien
+     //instanciamos un objeto huespedDataTest que recibe un objeto conexion
      huespedDataTest = new HuespedData(conexion);
+     //usamos Scanner para que pueda usar la consola de java para recibir valores por teclado
+     Scanner scanner = new Scanner(System.in);
      
+     //pedimos valores para el huesped
+     System.out.println("--Nuevo huesped--");
+     System.out.print("Ingrese dni: ");
+     long dni=Long.parseLong(scanner.nextLine());
+     System.out.print("Ingrese nombre: ");
+     String nombre = scanner.nextLine();
+     System.out.print("Ingrese domicilio: ");
+     String domicilio = scanner.nextLine();
+     System.out.print("Ingrese correo: ");
+     String correo = scanner.nextLine();
+     System.out.print("Ingrese celular: ");
+     String celular = scanner.nextLine();
+     
+     //una vez que recibimos todos los valores
+     //instanciamos un objeto huesped con los valores ingresados
+     //en el constructor
+     Huesped huesped = new Huesped(dni, nombre, domicilio, correo, celular);
+     //por ultimo usamos el metodo crear huesped de la clase Huesped data
+     // a traves del objeto huespedDataTest
+     huespedDataTest.crearHuesped(huesped);
         
   
      
