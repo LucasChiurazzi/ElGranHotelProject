4bf1bc179ba3a6d5c26fe001c405352240272b25
LucasChiurazzi <lucaschiurazzi@gmail.com>
2018-10-15 12:56:21 -0300

    Empezando con reserva

---
+++ src/elgranhotel/ReservaData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elgranhotel;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ReservaData {
+    private Connection connection = null;
+
+    public ReservaData(Conexion conexion) {
+        try {
+            connection=conexion.getConexion();
+        } catch (SQLException ex) {
+            System.out.println("Error al abrir al obtener la conexion");
+        }
+}
+}
